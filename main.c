d33c1167cb338ae5a7f2f9687ddc570cd23397d9
Alienxbe <martin.ykman@gmail.com>
2021-05-30 00:10:48 +0200

    Problems on negative exponent value

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mykman <mykman@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/03 20:34:49 by mykman            #+#    #+#             */
-/*   Updated: 2021/05/28 17:37:55 by mykman           ###   ########.fr       */
+/*   Updated: 2021/05/30 00:10:26 by mykman           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,29 +40,28 @@ int	main(void)
 {
 	t_float_cast	n;
 	int				m_size = 23;
-	//int				e_size = 8;
+	int				e_size = 8;
 	unsigned long	mantisa_val; // With a 1 bit before everything
-	unsigned long	exponent_val;
+	long	exponent_val;
 	//unsigned long	real_dec;
 	unsigned long 	float_dec;
 	int bit;
 
-	n.f = 0.3;
+	n.f = 0.1526;
 	mantisa_val = (n.s_parts.mantisa | 1L << m_size);
 	exponent_val = n.s_parts.exponent - 127;
-/*
-** 	printf("%d | %lu\n", n.s_parts.exponent - (1 << (e_size - 1)) - 2, exponent_val);
-** 	printf("Mantisa encoded: %u\n", n.s_parts.mantisa);
-** 	printf("Sign: %d\nReal value: %lu\n\n", n.s_parts.sign, mantisa_val >> (m_size - exponent_val));
-*/
-	bit = exponent_val + 1;
+	printf("%d | %ld\n", n.s_parts.exponent - (1 << (e_size - 1)) - 2, exponent_val);
+	printf("Mantisa encoded: %u\n", n.s_parts.mantisa);
+	printf("Sign: %d\nReal value: %lu\n\n", n.s_parts.sign, mantisa_val >> (m_size - exponent_val));
+	bit = m_size - exponent_val - 1;
 	float_dec = 0;
 	while (bit > 0)
 	{
-		if ((n.s_parts.mantisa >> (bit - 1)) & 1)
-			float_dec += ft_pow(5, exponent_val - bit + 2) * ft_pow(10, bit - 1);
+		printf("%d", (n.s_parts.mantisa >> (bit)) & 1);
+		if ((n.s_parts.mantisa >> (bit)) & 1)
+			float_dec += ft_pow(5, m_size - exponent_val - bit) * ft_pow(10, bit - 1);
 		bit--;
 	}
-	printf("%lu.%lu\n", mantisa_val >> (m_size - exponent_val), float_dec);
+	printf("\n%lu.%lu\n", mantisa_val >> (m_size - exponent_val), float_dec);
 	return (0);
 }
